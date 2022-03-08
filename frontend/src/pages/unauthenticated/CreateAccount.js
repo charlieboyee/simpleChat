67bf33f4bc8b0a24cc4d0d146493126d6debd984
Charles Park <charlieboyee@gmail.com>
2022-03-08 01:01:59 -0800

    small change

--- frontend/src/pages/unauthenticated/CreateAccount.js
+++ frontend/src/pages/unauthenticated/CreateAccount.js
@@ -14,8 +14,11 @@ export default function CreateAccount() {
 
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
+	const [usernameError, setUsernameError] = useState(false);
+	const [passwordError, setPasswordError] = useState(false);
 
-	const createAccount = async () => {
+	const createAccount = async (e) => {
+		e.preventDefault();
 		const result = await fetch('/api/createAccount', {
 			method: 'POST',
 			headers: {
@@ -24,15 +27,17 @@ export default function CreateAccount() {
 			body: JSON.stringify({ username, password }),
 		});
 		if (result.status === 200) {
-			const response = await result.json();
-			console.log(response);
-			return;
+			const { status } = await result.json();
+			if (status) return navigate('/', { replace: true });
+			setUsernameError(true);
+			return console.log('User already exists');
 		}
 		console.log('there is an error');
 		return;
 	};
 
 	const handleUsernameChange = (e) => {
+		setUsernameError(false);
 		setUsername(e.target.value);
 	};
 
@@ -45,18 +50,23 @@ export default function CreateAccount() {
 			<Card>
 				<CardHeader title='Create Account' />
 				<CardContent>
-					<TextField
-						value={username}
-						onChange={handleUsernameChange}
-						placeholder='Username'
-					/>
-					<TextField
-						value={password}
-						onChange={handlePasswordChange}
-						placeholder='Password'
-						type='password'
-					/>
-					<Button onClick={createAccount}>Create</Button>
+					<form onSubmit={createAccount}>
+						<TextField
+							error={usernameError}
+							helperText='Username already exists'
+							value={username}
+							onChange={handleUsernameChange}
+							placeholder='Username'
+						/>
+						<TextField
+							error={passwordError}
+							value={password}
+							onChange={handlePasswordChange}
+							placeholder='Password'
+							type='password'
+						/>
+						<Button type='submit'>Create</Button>
+					</form>
 				</CardContent>
 				<CardActions>
 					<Button onClick={() => navigate('/', { replace: true })}>
